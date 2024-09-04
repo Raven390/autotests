d01e0630fd335696764243c3279d548e102dd81a
Nikolai Koriagin <nikolai.koriagin@hytechc.com>
2024-09-04 09:24:33 +0000

    Add Constants.java

---
+++ src/main/java/helpers/Constants.java
@@ -0,0 +1,19 @@
+package helpers;
+
+public class Constants {
+    // TEAMS
+    public static final String TEAM_CORE = "team_core";
+    public static final String TEAM_BACKOFFICE = "team_backoffice";
+    // OWNERS
+    public static final String OWNER_NIKOLAI_KORIAGIN = "owner_nikolai_koriagin";
+    public static final String OWNER_DMITRI_KALACHEV = "owner_dmitri_kalachev";
+    // TEST CASE STATUSES
+    public static final String STATUS_MANUAL = "status_manual";
+    public static final String STATUS_AUTOMATED = "status_automated";
+    // TEST LAYERS
+    public static final String LAYER_API = "layer_api";
+    public static final String LAYER_WEB = "layer_web";
+    public static final String LAYER_MOBILE = "layer_mobile";
+    // FEATURES
+    public static final String FEATURE_LEXIS_NEXIS = "feature_lexis_nexis";
+}
