eb28bdaf53f7beb464dd343889260ce9cf504e39
Nikolai Koriagin <nikolai.koriagin@hytechc.com>
2024-09-10 07:55:51 +0000

    Make headless depend from env

--- src/main/java/utils/ConfigFactory.java
+++ src/main/java/utils/ConfigFactory.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.Objects;
 import org.aeonbits.owner.Config;
 
 public class ConfigFactory {
@@ -17,9 +18,9 @@ public class ConfigFactory {
             KAFKA_PORT = CONFIG.kafkaPort(),
             KAFKA_CORE_INCOMING_TOPIC = CONFIG.kafkaCoreIncomingTopic(),
             KAFKA_CORE_OUTCOMING_TOPIC = CONFIG.kafkaCoreOutcomingTopic(),
-            BOKAFKAPUBLIC = CONFIG.BOKafkaPublic(),
-            BOKAFKAPRIVATE = CONFIG.BOKafkaPrivate(),
-            BOKAFKAPASS = CONFIG.BOKafkaPass(),
+            BACK_OFFICE_KAFKA_PUBLIC = CONFIG.backOfficeKafkaPublic(),
+            BACK_OFFICE_KAFKA_PRIVATE = CONFIG.backOfficeKafkaPrivate(),
+            BACK_OFFICE_KAFKA_PASSWORD = CONFIG.backOfficeKafkaPassword(),
 
             // Databases
             POSTGRE_HOST = CONFIG.postgreLogin(),
@@ -38,8 +39,6 @@ public class ConfigFactory {
             REGISTRATION_HELPER_REG_INTERFACE = CONFIG.registrationHelperRegistrationInterface(),
             // Other
             COUNTRY_MALAYSIA = CONFIG.countryMalaysia();
-
-    public static final Boolean HEADLESS = CONFIG.headlessMode();
 
     @Config.Sources({"classpath:config/config.properties", "system:properties"})
     public interface UserConfig extends Config {
@@ -77,14 +76,14 @@ public class ConfigFactory {
         @Key("kafkaCoreOutcomingTopic")
         String kafkaCoreOutcomingTopic();
 
-        @Key("BOKafkaPublic")
-        String BOKafkaPublic();
+        @Key("backOfficeKafkaPublic")
+        String backOfficeKafkaPublic();
 
-        @Key("BOKafkaPrivate")
-        String BOKafkaPrivate();
+        @Key("backOfficeKafkaPrivate")
+        String backOfficeKafkaPrivate();
 
-        @Key("BOKafkaPass")
-        String BOKafkaPass();
+        @Key("backOfficeKafkaPassword")
+        String backOfficeKafkaPassword();
 
         // Databases
 
@@ -133,5 +132,19 @@ public class ConfigFactory {
 
         @Key("countryMalaysia")
         String countryMalaysia();
+    }
+
+    public static boolean isGitlab() {
+        return Objects.equals(System.getenv("IS_GITLAB_CI"), "true");
+    }
+
+    public static Boolean getHeadless() {
+        if (isGitlab()) {
+            System.out.println("Set Headless mode to 'true'");
+            return true;
+        } else {
+            System.out.println("Set Headless mode to local value in config.properties");
+            return CONFIG.headlessMode();
+        }
     }
 }
