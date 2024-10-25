3d15e40da1579b4652dc09924c5628586776dc99
Nikolai Koriagin <nikolai.koriagin@hytechc.com>
2024-10-25 08:29:33 +0000

    Disable-lexis-nexis-test

--- src/test/java/tests/uiTests/core/LexisNexisVantageIntegrationTest.java
+++ src/test/java/tests/uiTests/core/LexisNexisVantageIntegrationTest.java
@@ -6,19 +6,21 @@ import static utils.Utils.*;
 
 import helpers.database.MySqlHelper;
 import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.junit.jupiter.api.*;
 import tests.TestBaseWeb;
 
+@Feature(FEATURE_LEXIS_NEXIS)
+@Tag(TEAM_CORE)
 public class LexisNexisVantageIntegrationTest extends TestBaseWeb {
 
+    @Disabled
     @Test
     @Owner(OWNER_NIKOLAI_KORIAGIN)
-    @Tag(TEAM_CORE)
     @Tag(STATUS_AUTOMATED)
-    @Tag(FEATURE_LEXIS_NEXIS)
     @Tag(LAYER_API)
     @DisplayName("Vantage. Registration Lexis Nexis event")
     @AllureId("57")
