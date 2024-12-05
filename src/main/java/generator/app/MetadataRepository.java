5d15edadf5395a4f3a6d16d39df6c0655a880f0c
Nikolai Kalgin <nikolai.kalgin@hytechc.com>
2024-12-05 19:01:53 +0200

    CSV-450. Test data generation API

---
+++ src/main/java/generator/app/MetadataRepository.java
@@ -0,0 +1,70 @@
+package generator.app;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+final class MetadataRepository {
+
+    public record Result(Object value, Timestamp timestamp) {
+    }
+
+    private final Connection connection;
+    private final ObjectMapper objectMapper;
+
+    public MetadataRepository(Connection connection, ObjectMapper objectMapper) {
+        this.connection = connection;
+        this.objectMapper = objectMapper;
+
+        try (var st = connection.createStatement()) {
+            st.executeUpdate("CREATE TABLE IF NOT EXISTS result (key TEXT, value TEXT, created INTEGER)");
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public Optional<Result> find(String name, TypeReference<?> asType) throws IllegalStateException {
+        try (var st = connection.prepareStatement("SELECT value, created FROM result WHERE key = ? ORDER BY created DESC")) {
+            st.setString(1, name);
+            var rs = st.executeQuery();
+            if (!rs.isBeforeFirst()) {
+                return Optional.empty();
+            }
+            var value = asType == null ? rs.getString("value") : objectMapper.readValue(rs.getString("value"), asType);
+            return Optional.of(new Result(value, rs.getTimestamp("created")));
+        } catch (SQLException | JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public Optional<Result> find(String name) {
+        return find(name, null);
+    }
+
+    public void save(String name, Object value) throws IllegalStateException {
+        try (var st = connection.prepareStatement("INSERT INTO result (key, value, created) VALUES (?, ?, ?)")) {
+            st.setString(1, name);
+            st.setString(2, objectMapper.writeValueAsString(value));
+            st.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            st.executeUpdate();
+        } catch (SQLException | JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public void remove(String name, Timestamp timestamp) throws IllegalStateException {
+        try (var st = connection.prepareStatement("DELETE FROM result WHERE name = ? AND created = ?")) {
+            st.setString(1, name);
+            st.setTimestamp(2, timestamp);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
---
+++ src/main/java/generator/app/RulesHandler.java
@@ -0,0 +1,56 @@
+package generator.app;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.lang.reflect.Modifier.isStatic;
+
+final class RulesHandler {
+
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+    private final RuleTypeOperations typeOperations;
+    private final MetadataRepository metadataRepository;
+
+    public RulesHandler(RuleTypeOperations typeOperations, MetadataRepository metadataRepository) {
+        this.typeOperations = typeOperations;
+        this.metadataRepository = metadataRepository;
+    }
+
+    public Optional<RuleTypeOperations.MethodInfo> create(String name) throws IllegalStateException {
+        var setupInfo = typeOperations.getMethodInfo(name, m -> isStatic(m.getModifiers()) && m.getParameterCount() == 0 && m.getName().startsWith("setup"));
+        setupInfo.ifPresent(info -> executorService.execute(() -> {
+            try {
+                var returnValue = info.method().invoke(null);
+                metadataRepository.save(name, returnValue);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new IllegalStateException(e);
+            }
+        })
+        );
+        return setupInfo;
+    }
+
+    public Optional<RuleTypeOperations.MethodInfo> delete(String name) throws IllegalStateException {
+        var deleteInfo = typeOperations.getMethodInfo(name, m -> isStatic(m.getModifiers()) && m.getParameterCount() == 1 && m.getName().startsWith("delete"));
+        deleteInfo.ifPresent(info -> executorService.execute(() -> metadataRepository.find(name, info.paramTypeRefs()[0]).ifPresent(rule -> {
+            try {
+                info.method().invoke(null, rule.value());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new IllegalStateException(e);
+            }
+            metadataRepository.remove(name, rule.timestamp());
+        })));
+        return deleteInfo;
+    }
+
+    public Collection<String> getAll() {
+        return typeOperations.getAllRuleNames();
+    }
+
+    public Optional<Object> getOne(String name) {
+        return metadataRepository.find(name).map(MetadataRepository.Result::value);
+    }
+}
