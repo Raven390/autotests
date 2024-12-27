7533fff6d76b2183965657324832ee75ea1bf30e
Nikolai Koriagin <nikolai.koriagin@hytechc.com>
2024-12-27 12:33:20 +0000

    CSV-551-unclosed-trades

---
+++ src/main/java/businessObjects/api/clickhouseApiService/getUnclosedTrades/GetUnclosedTradesResponse.java
@@ -0,0 +1,26 @@
+package businessObjects.api.clickhouseApiService.getUnclosedTrades;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class GetUnclosedTradesResponse {
+
+    @JsonProperty("openTime")
+    public String tradeDate;
+
+    @JsonProperty("tradeId")
+    public Integer tradeId;
+
+    @JsonProperty("tradingAccount")
+    public Integer tradingAccount;
+
+    @JsonProperty("profit")
+    public Double profit;
+
+    @JsonProperty("profitUSD")
+    public Double profitUsd;
+
+    @JsonProperty("comment")
+    public String comment;
+
+}
