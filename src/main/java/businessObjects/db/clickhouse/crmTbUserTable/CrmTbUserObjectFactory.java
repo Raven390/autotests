6009c22af1af1b0b1832c5cbe1c758b9794679ac
Nikolai Koriagin <nikolai.koriagin@hytechc.com>
2025-01-28 09:59:20 +0000

    Fix nigthly test

--- src/main/java/businessObjects/db/clickhouse/crmTbUserTable/CrmTbUserObjectFactory.java
+++ src/main/java/businessObjects/db/clickhouse/crmTbUserTable/CrmTbUserObjectFactory.java
@@ -9,10 +9,10 @@ import static utils.Utils.getRandomUuidString;
 public class CrmTbUserObjectFactory {
     @Step("Generate user object by Client")
     public static CrmTbUserObject generateUserByClient(ClientHelper client) {
-        return new CrmTbUserObject(client.getUserId(), client.getUcid(), client.getBrand(), client.getRegulator(), "2024-10-23", "Test", "User", "1", "1961-02-01", "Cyprus", "CY", "CY", "en", "RUS", "DUrksdLPlqZB6byC9vfKk6qm9BpUmsOS", "BjrbbdAHkwhBFLnPclfvbg==", "357", "1", "2FA", "2", "1", "2", client.getIbId(), client.getCpaId(), client.getReferrerId(), "PARTIAL_KYC_ID_PASS", "2024-10-29 09:55:01.300", "2024-10-23 14:56:59.000", getRandomUuidString(), "nationalityId");
+        return new CrmTbUserObject(client.getUserId(), client.getUcid(), client.getBrand(), client.getRegulator(), "2024-10-23", "Test", "User", "1", "1961-02-01", "Cyprus", "CY", "CY", "en", "RUS", "DUrksdLPlqZB6byC9vfKk6qm9BpUmsOS", "BjrbbdAHkwhBFLnPclfvbg==", "357", "1", "2FA", "2", "1", "2", client.getIbId(), client.getCpaId(), client.getReferrerId(), "PARTIAL_KYC_ID_PASS", "2024-10-29 09:55:01.300", "2024-10-23 14:56:59.000", getRandomUuidString(), "nationalityId", "2024-11-29 09:55:01");
     }
 
     public static CrmTbUserObject generateStaticUserByClient(ClientHelper client) {
-        return new CrmTbUserObject(client.getUserId(), client.getUcid(), client.getBrand(), client.getRegulator(), "2024-10-23", "Test", "User", "1", "1961-02-01", "Cyprus", "CY", "CY", "en", "RUS", "DUrksdLPlqZB6byC9vfKk6qm9BpUmsOS", "BjrbbdAHkwhBFLnPclfvbg==", "357", "1", "2FA", "2", "1", "2", 1, 2, 3, "PARTIAL_KYC_ID_PASS", "2024-10-29 09:55:01.300", "2024-10-23 14:56:59.000", "063cde3b-ea9d-48b5-8e2c-99f3d5f67999", "nationalityId");
+        return new CrmTbUserObject(client.getUserId(), client.getUcid(), client.getBrand(), client.getRegulator(), "2024-10-23", "Test", "User", "1", "1961-02-01", "Cyprus", "CY", "CY", "en", "RUS", "DUrksdLPlqZB6byC9vfKk6qm9BpUmsOS", "BjrbbdAHkwhBFLnPclfvbg==", "357", "1", "2FA", "2", "1", "2", 1, 2, 3, "PARTIAL_KYC_ID_PASS", "2024-10-29 09:55:01.300", "2024-10-23 14:56:59.000", "063cde3b-ea9d-48b5-8e2c-99f3d5f67999", "nationalityId", "2024-11-29 09:55:01");
     }
 }
