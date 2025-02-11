ac83becc9ab7085bba3d3c7c69f2b7811ab9c15d
Fedor Nesterovich <fedor.nesterovich@hytechc.com>
2025-02-11 14:20:11 +0000

    CSV-841. NBP Losing leg rule

--- src/main/java/businessObjects/db/clickhouse/aggrMirrorAccountsByTrades/MirrorLoginObjectFactory.java
+++ src/main/java/businessObjects/db/clickhouse/aggrMirrorAccountsByTrades/MirrorLoginObjectFactory.java
@@ -11,4 +11,9 @@ public class MirrorLoginObjectFactory {
     public static MirrorLoginObject generateMirrorTradesByAccount(ClientHelper client) {
         return new MirrorLoginObject("EURUSD", client.getTradingAccount().toString(), client.getServerId().toString(), 1.1, String.valueOf(1234), String.valueOf(2), 3.3, getCurrentTimestampDbFormat());
     }
+
+    @Step("Generate mirror trades client from to")
+    public static MirrorLoginObject generateMirrorTradesByClients(ClientHelper clientFrom, ClientHelper clientTo) {
+        return new MirrorLoginObject("EURUSD", clientFrom.getTradingAccount().toString(), clientFrom.getServerId().toString(), 1.1, clientTo.getTradingAccount().toString(), clientTo.getServerId().toString(), 3.3, getCurrentTimestampDbFormat());
+    }
 }
--- src/main/java/businessObjects/db/clickhouse/mirrorUcidTable/MirrorUcidObjectFactory.java
+++ src/main/java/businessObjects/db/clickhouse/mirrorUcidTable/MirrorUcidObjectFactory.java
@@ -7,8 +7,13 @@ import io.qameta.allure.Step;
 import static utils.Utils.*;
 
 public class MirrorUcidObjectFactory {
-    @Step("Generate lexis nexis object by Client")
+    @Step("Generate mirror ucid object by Client")
     public static MirrorUcidObject generateMirrorUcidObjectByClient(ClientHelper client) {
         return new MirrorUcidObject("EURUSD", client.getUcid(), 1d, 1, client.getUcid() + 1, 1d, 1, 1f, 1f, 1f, 1f, 1f, 1f, getCurrentTimestampDbFormat());
+    }
+
+    @Step("Generate mirror ucid object by client from to")
+    public static MirrorUcidObject generateMirrorUcidObjectByClients(ClientHelper clientFrom, ClientHelper clientTo) {
+        return new MirrorUcidObject("EURUSD", clientFrom.getUcid(), 1d, 1, clientTo.getUcid(), 1d, 1, 1f, 1f, 1f, 1f, 1f, 1f, getCurrentTimestampDbFormat());
     }
 }
