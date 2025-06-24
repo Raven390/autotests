3df264afe11a2cc84910c96635d8ef628122f647
Dmitrii Kalachev <dmitrii.kalachev@hytechc.com>
2025-06-24 10:27:25 +0000

    Connection by attributes fix

--- src/main/java/business_objects/db/clickhouse/email_table/EmailTableEntryFactory.java
+++ src/main/java/business_objects/db/clickhouse/email_table/EmailTableEntryFactory.java
@@ -2,6 +2,8 @@ package business_objects.db.clickhouse.email_table;
 
 import helpers.data.ClientHelper;
 
+import static utils.Utils.getCurrentTimestamp;
+
 
 public class EmailTableEntryFactory {
 
@@ -13,7 +15,7 @@ public class EmailTableEntryFactory {
 
     public static EmailTableEntry emailTableEntryForConnectionSearchFiltration(ClientHelper client) {
         return new EmailTableEntry(
-                client.getUcid(), "testfiltration@qatest.com"
+                client.getUcid(), "testfiltration" + getCurrentTimestamp() + "@qatest.com"
         );
     }
 
