b4a736472a017cc384325f745478811825d887fb
Dmitrii Kalachev <dmitrii.kalachev@hytechc.com>
2025-07-28 13:04:54 +0000

    Resolve BO-07 "Fix  28"

--- src/test/java/tests/vindex_backoffice_ui_tests/abuseRegistry/MassDeleteTest.java
+++ src/test/java/tests/vindex_backoffice_ui_tests/abuseRegistry/MassDeleteTest.java
@@ -49,7 +49,7 @@ public class MassDeleteTest extends TestBaseWeb {
     @Tag(ABUSE_REGISTRY)
     @AllureId("1328")
     @DisplayName("Abuse registry mass delete full flow simple test")
-    public void abuseRegistryMassDeleteSimpleFullFlowTest() throws Exception {
+    void abuseRegistryMassDeleteSimpleFullFlowTest() throws Exception {
 
         cleanClientAudit(client1.getUcid(), client2.getUcid());
         deleteUserBO(client1.getUcid());
@@ -60,19 +60,75 @@ public class MassDeleteTest extends TestBaseWeb {
         cleanUserRestriction(client3.getUcid());
         deleteUserAR(client1.getUcid(), client2.getUcid(), client3.getUcid());
 
-        addFraudsForClient(client1, List.of(HEDGING, CPA_ABUSE), FraudTypeStatus.CONFIRMED);
-        addFraudsForClient(client2, List.of(HEDGING), FraudTypeStatus.CONFIRMED);
-        addFraudsForClient(client3, List.of(HEDGING), FraudTypeStatus.CONFIRMED);
+        addFraudsForClient(client1, List.of(HEDGING, CPA_ABUSE), FraudTypeStatus.POTENTIAL);
+        addFraudsForClient(client2, List.of(HEDGING), FraudTypeStatus.POTENTIAL);
+        addFraudsForClient(client3, List.of(HEDGING), FraudTypeStatus.POTENTIAL);
 
         investigationPage.navigateEnterPage();
         keycloackPage.loginAsAutotestUser();
         fraudstersPage.navigateAbuseRegistryFraudsters();
-        fraudstersPage.openUploadDrawer();
+        fraudstersPage.openRemoveDrawer();
         fraudstersPage.selectBrandToUpload(Brand.VANTAGE.getDisplayName());
         fraudstersPage.typeClientsID(client1.getUserId().toString(), client2.getUserId().toString(), client3.getUserId().toString());
         fraudstersPage.clickAddFraudButton();
         FraudTypeOld fraudTypeOld = FraudTypeOld.HEDGING;
-        fraudstersPage.addSelectedFraudFraud(fraudTypeOld.getDisplayName(), "Confirmed");
+        fraudstersPage.addSelectedFraudDelete(fraudTypeOld.getDisplayName());
+        String commentary = "test" + getCurrentTimestamp();
+        fraudstersPage.fillCommentary(commentary);
+        fraudstersPage.clickDeleteUpload();
+        fraudstersPage.verifySuccessMessageDelete();
+
+        page.waitForTimeout(1000);
+
+        List<AbuserFraudType> fraudsFirst = getObjectsFromDB(DbName.POSTGRES, "ar.abuser_fraud_type", "ucid='" + client1.getUcid() + "'", AbuserFraudType.class);
+        Allure.step("Assert that there two records in ar.abuser_fraud_type for the first client");
+        assertEquals(2, fraudsFirst.size());
+
+        Allure.step("Find among frauds of firs user fraud with time CPA Abuse (that that we not deleted)");
+
+        AbuserFraudType fraudFirst = fraudsFirst.stream().filter(fraud -> fraud.getFraudTypeCode().equals(CPA_ABUSE.getCode())).findFirst().orElse(null);
+        Assertions.assertNotNull(fraudFirst);
+
+        Allure.step("Assert that record in ar.abuser_fraud_type have right status");
+        assertEquals("POTENTIAL", fraudFirst.getStatus());
+
+        Allure.step("Find among frauds of firs user fraud with time CPA Abuse (that that we not deleted)");
+
+        AbuserFraudType fraudSecond = fraudsFirst.stream().filter(fraud -> fraud.getFraudTypeCode().equals(HEDGING.getCode())).findFirst().orElse(null);
+        Assertions.assertNotNull(fraudSecond);
+
+        Allure.step("Assert that record in ar.abuser_fraud_type have right status");
+        assertEquals("CLEANED", fraudSecond.getStatus());
+    }
+
+    @Test
+    @Tag(TEAM_BACKOFFICE)
+    @Tag(LAYER_WEB)
+    @Tag(ABUSE_REGISTRY)
+    @AllureId("1377")
+    @DisplayName("Abuse registry mass delete can delete only potential")
+    void abuseRegistryMassCanDeleteOnlyPotential() throws Exception {
+
+        cleanClientAudit(client1.getUcid(), client2.getUcid());
+        deleteUserBO(client1.getUcid());
+        cleanUserRestriction(client1.getUcid());
+        deleteUserAR(client1.getUcid());
+
+        addFraudsForClient(client1, List.of(HEDGING), FraudTypeStatus.POTENTIAL);
+        addFraudsForClient(client1, List.of(CPA_ABUSE), FraudTypeStatus.CONFIRMED);
+
+        investigationPage.navigateEnterPage();
+        keycloackPage.loginAsAutotestUser();
+        fraudstersPage.navigateAbuseRegistryFraudsters();
+        fraudstersPage.openRemoveDrawer();
+        fraudstersPage.selectBrandToUpload(Brand.VANTAGE.getDisplayName());
+        fraudstersPage.typeClientsID(client1.getUserId().toString());
+        fraudstersPage.clickAddFraudButton();
+        FraudTypeOld fraudTypeOld1 = FraudTypeOld.HEDGING;
+        FraudTypeOld fraudTypeOld2 = FraudTypeOld.CPA_ABUSE;
+        fraudstersPage.addSelectedFraudDelete(fraudTypeOld1.getDisplayName());
+        fraudstersPage.clickAddFraudButton();
+        fraudstersPage.addSelectedFraudDelete(fraudTypeOld2.getDisplayName());
         String commentary = "test" + getCurrentTimestamp();
         fraudstersPage.fillCommentary(commentary);
         fraudstersPage.clickDeleteUpload();
